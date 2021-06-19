5952fac11dead1c81c8f71c45500ee004513fa2d
huynp1999 <83684068+huynp1999@users.noreply.github.com>
2021-06-19 11:22:44 +0700

    Update tc.sh

--- Network/Traffic-control/Lab2/tc.sh
+++ Network/Traffic-control/Lab2/tc.sh
@@ -2,12 +2,12 @@
 if="ens3"
 
 tc q del dev $if root
-
 tc q add dev $if root handle 1:0 htb default 10
-tc c add dev $if parent 1:0 classid 1:1 htb rate 20mbit
+tc c add dev $if parent 1:0 classid 1:3 htb rate 10mbps
 
-tc c add dev $if parent 1:1 classid 1:3 htb rate 10mbps
+#fastlink
 tc c add dev $if parent 1:3 classid 1:31 htb rate 8mbps ceil 10mbps prio 0
+#slowlink
 tc c add dev $if parent 1:3 classid 1:33 htb rate 1mbps ceil 2mbps pri 1
 
 tc f add dev $if parent 1:0 protocol ip prio 9 u32 match ip dst 192.168.1.10 flowid 1:31
