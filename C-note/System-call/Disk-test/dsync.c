f7d17556a56b6cb37a59179b1097afccbc225d72
huynp1999 <83684068+huynp1999@users.noreply.github.com>
2021-07-22 16:54:29 +0700

    Create dsync.c

---
+++ C-note/System-call/Disk-test/dsync.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/timeb.h>
+#include <string.h>
+
+#define MEGABYTE (1024*1024)
+
+int main(int argc, char * argv[]){
+    long i, buf_size;
+    int fo, ret;
+    // unsigned char *buf;
+    char *buf;
+    struct timeb start,end;
+    srand(time(NULL));
+
+    ftime(&start);
+
+    fo = open(argv[1], O_CREAT | O_WRONLY | O_DSYNC);
+    buf_size=atof(argv[2]);
+    buf= (char *) malloc (buf_size); //cap phat bo nho buffer dua tren dung luong file
+
+    memset(buf, 'A', buf_size); //ghi random du lieu vao buffer de sau day vao file
+    
+    ret= write(fo,buf,buf_size); //ghi du lieu tu buffer vao file
+    //fdatasync(fo);
+    ftime(&end);
+
+    float write_time=end.time-start.time; //tinh thoi gian ghi theo giay
+    if (write_time < 1)
+        printf("Write rate: %.2f KB/s\n\nExecute time: < 0 \n", buf_size/1000.0);
+    else
+        printf ("Write rate: %.2f MB/s\nExecute time: %.2f s\n", (ret/MEGABYTE)/write_time, write_time);
+
+    free(buf);
+    close(fo);
+    return 0;
+}
